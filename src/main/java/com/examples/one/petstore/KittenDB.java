15c11cbc2cdbf6a741eb5e5e3228810dabe7d53b
Sara Ann Abdilla <saraabdilla274@gmail.com>
2016-03-14 09:28:36 +0100

    DBs

---
+++ src/main/java/com/examples/one/petstore/KittenDB.java
@@ -0,0 +1,28 @@
+package src.main.java.com.examples.one.petstore;
+
+import java.util.*;
+
+public class KittenDB
+{
+    ArrayList<Kitten> Kittens = new ArrayList();
+    
+    boolean addKitten(Kitten k)
+    {
+        return true;
+    }
+    
+    boolean delKitten(int id)
+    { 
+        return true;
+    }
+    
+    Kitten getKitten(int id)
+    {
+        return Kitten.id;
+    }
+    
+    int countKittens()
+    {
+        return 5;
+    }
+}
---
+++ src/main/java/com/examples/one/petstore/PuppyDB.java
@@ -0,0 +1,28 @@
+package src.main.java.com.examples.one.petstore;
+
+import java.util.*;
+
+public class PuppyDB
+{
+    ArrayList<Puppy> Puppies = new ArrayList();
+    
+    boolean addPuppy(Puppy p)
+    {
+        return true;
+    }
+    
+    boolean delPuppy(int id)
+    { 
+        return true;
+    }
+    
+    Puppy getPuppy(int id)
+    {
+        return Puppy.id;
+    }
+    
+    int countPuppies()
+    {
+        return 5;
+    }
+}
