c2d7b0cf867d0775e26ed48e54ec6f12597ca9a6
Roberta Caruana <caruana.rob@gmail.com>
2016-03-14 09:26:20 +0100

    Class Kitten.java

---
+++ src/main/java/com/examples/one/petstore/Kitten.java
@@ -0,0 +1,51 @@
+package com.examples.one.petstore;
+
+/**
+ * Created by Bobby on 14-Mar-16.
+ */
+public class Kitten {
+    int id;
+    String name;
+    String[] breed = {"Persian", "Siamese", "Bengal"};
+    Boolean sensitiveToFeeding;
+
+    public Kitten(int id, String name, String[] breed)
+    {
+        this.id = id;
+        this.name = name;
+        this.breed = breed;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBreed(String[] breed) {
+        this.breed = breed;
+    }
+
+    public void setSensitiveToFeeding(Boolean sensitiveToFeeding) {
+        this.sensitiveToFeeding = sensitiveToFeeding;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String[] getBreed() {
+        return breed;
+    }
+
+    public Boolean getSensitiveToFeeding() {
+        return sensitiveToFeeding;
+    }
+
+}
