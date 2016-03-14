aaad3e1778275dc655815c1eadc2bd396b4a200c
Leanne Marie Vassallo <leanne.vassallo.14@um.edu.mt>
2016-03-14 09:31:50 +0000

    New Puppy class

--- src/main/java/com/examples/one/petstore/Puppy.java
+++ src/main/java/com/examples/one/petstore/Puppy.java
@@ -1,7 +1,56 @@
 package com.examples.one.petstore;
-
-/**
- * Created by leann on 14/03/2016.
- */
 public class Puppy {
+
+    int id;
+    String name;
+    String[] breed = {"Labrador", "Doberman", "Pitbull"};
+    Boolean loudBark;
+    Boolean friendlyWithChildren;
+
+    public Puppy (int id, String name, String breed []){
+        this.id = id;
+        this.name = name;
+        this.breed = breed;
+    }
+
+    public String[] getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String[] breed) {
+        this.breed = breed;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public Boolean getLoudBark() {
+        return loudBark;
+    }
+
+    public void setLoudBark(Boolean loudBark) {
+        this.loudBark = loudBark;
+    }
+
+    public Boolean getFriendlyWithChildren() {
+        return friendlyWithChildren;
+    }
+
+    public void setFriendlyWithChildren(Boolean friendlyWithChildren) {
+        this.friendlyWithChildren = friendlyWithChildren;
+    }
 }
